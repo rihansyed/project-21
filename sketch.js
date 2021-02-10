583c0412ae328899d6388be9b9f77629bcb015ed
rihansyed <77448053+rihansyed@users.noreply.github.com>
2021-02-10 11:03:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var car,bullet,wall;
+var speed,weight,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+thickness=random(22,83)
+
+bullet=  createSprite(50, 200, 50, 5);
+
+bullet.shapeColor="white";
+
+wall= createSprite(1200,200,thickness,height/2);
+
+speed=random(223,321)
+weight=random(30,52)
+
+bullet.velocityX=speed;
+}
+
+function draw() {
+  background("black");  
+
+
+if(wall.x-bullet.x <(bullet.width+wall.width)/2){
+
+bullet.velocityX=0;
+var deformation=0.5*weight*speed*speed/22500;
+if(deformation<100){
+  bullet.shapeColor="white";
+}
+if(deformation<180 && deformation>100){
+  bullet.shapeColor="white";
+}
+if(deformation>180){
+  bullet.shapeColor="white";
+}
+
+}
+
+if (Rihan(bullet,wall)){
+
+var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+if (damage>10){
+  wall.shapeColor="red";
+ }
+ 
+if (damage<10){
+wall.shapeColor="green";
+ }
+
+}
+
+  drawSprites();
+}
+
+function Rihan(lbullet,lwall){
+
+bulletRightEdge=lbullet.x +lbullet.width;
+wallLeftEdge=lwall.x;
+if (bulletRightEdge>=wallLeftEdge){
+  return true
+}
+  return false;
+}
